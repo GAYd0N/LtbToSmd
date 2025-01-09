bdefa72f2e74c06bfc828dfa3c65c0a1860d9a1e
GAYd0N <107982248+GAYd0N@users.noreply.github.com>
2025-01-09 12:06:05 +0800

    添加项目文件。

---
+++ LtbToSmd/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Input;
+using CommunityToolkit.Mvvm.Input;
+
+namespace LtbToSmd.ViewModels
+{
+    public partial class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting { get; } = "Welcome to Avalonia!";
+
+    }
+}
---
+++ LtbToSmd/Views/MainWindow.axaml.cs
@@ -0,0 +1,15 @@
+using Avalonia.Controls;
+using Avalonia.Input;
+
+
+namespace LtbToSmd.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
